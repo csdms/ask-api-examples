1aa1bdbf35e90eb83c6006ada71434057bd0e75e
Mark Piper <mark.piper@colorado.edu>
2016-02-21 21:46:35 -0700

    Add example of using Python to make Ask API call
    The example is to get all the models written by the user with the last
    name "Tucker".

---
+++ csdms_wiki_api_examples/__init__.py
@@ -0,0 +1,16 @@
+import urllib
+
+
+base_url = 'http://csdms.colorado.edu/mediawiki/api.php?'
+params = {'action': 'ask', 'format': 'json'}
+
+
+def call_api(query='[[Model:HydroTrend]]'):
+    params['query'] = query
+    fp = urllib.urlopen(base_url, urllib.urlencode(params))
+    return fp.read()
+
+
+def write_file(json_str, file_name):
+    with open(file_name + '.json', 'w') as fp:
+        fp.write(json_str)
---
+++ csdms_wiki_api_examples/list_users_models.py
@@ -0,0 +1,14 @@
+import os
+from csdms_wiki_api_examples import call_api, write_file
+
+
+def main():
+    query = '[[Last name::Tucker]]'
+    r = call_api(query)
+    base_file_name, ext = os.path.splitext(__file__)
+    write_file(r, base_file_name)
+    print r
+
+
+if __name__ == '__main__':
+    main()
